3c742cfeaf50deb7c606b6773b268c588d3a9671
Bookaura <tahia9811@gmail.com>
2025-06-25 00:03:35 +0600

    First Commit

---
+++ Desktop/WordWizard/WordWizard/WordWizard/src/wordwizard/WordWizard/DashBoardController.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package wordwizard.WordWizard;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ovi
+ */
+public class DashBoardController implements Initializable {
+
+    @FXML
+    private Text WelSlogan;
+    @FXML
+    private Button LetsPlay;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        LetsPlay.setOnAction(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+                Parent root = loader.load();
+
+                // Get current stage and set new scene
+                Stage stage = (Stage) LetsPlay.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Login");
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }    
+    
+}
